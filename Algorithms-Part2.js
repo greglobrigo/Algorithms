1facd6ed47cd98e1d3af5eeeb9e4471abe9ffbbd
greglobrigo <golivefun@gmail.com>
2023-11-15 22:10:30 +0800

    Removed comment.

--- Algorithms-Part2.js
+++ Algorithms-Part2.js
@@ -100,7 +100,6 @@ function twoSum(nums, target) {
 }
 
 //Given a positive integral number n, return a strictly increasing sequence (list/array/string depending on the language) of numbers, so that the sum of the squares is equal to n².
-//If there are multiple solutions (and there will be), return as far as possible the result with the largest possible values:
 
 function decompose(n) {
     let result = []
